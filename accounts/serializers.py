e5b527159f56aa07c834eff259d4c47a9254d47e
Amir Pashayi <amir.pasha.dev@gmail.com>
2025-09-18 16:42:02 +0330

    update user serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,12 +1,31 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id' ,'username' ,'email' ,'first_name' ,'last_name' ,'password')
+        fields = ('id','username','email','first_name','last_name','password')
         read_only_fields = ('id',)
-        write_only_fields = ('password',)
+        extra_kwargs = {
+            'password': {'write_only': True, 'required': True},
+            'email': {'required': False, 'allow_blank': True},
+            'first_name': {'required': False, 'allow_blank': True},
+            'last_name': {'required': False, 'allow_blank': True},
+        }
 
     def create(self, validated_data):
-        return User.objects.create_user(**validated_data)
+        password = validated_data.pop('password')
+        user = User.objects.create_user(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
